2835edeeb87578d53a29a478aec11a8be0648a66
kdp <kdp7584@gmail.com>
2020-09-18 20:15:10 +0900

    Create WebViewContainer.m

---
+++ Common/WebViewContainer.m
@@ -0,0 +1,41 @@
+#import "WebViewContainer.h"
+#import <WebKit/WebKit.h>
+
+@implementation WebViewContainer
+
+
++ (void)setURL:(NSString *)requestURLString:(WKWebView *) webView {
+    NSURL *url = [[NSURL alloc] initWithString: requestURLString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL: url
+                                                  cachePolicy: NSURLRequestUseProtocolCachePolicy
+                                              timeoutInterval: 5];
+    
+    [webView loadRequest: request];
+}
+
++ (WKWebViewConfiguration *)setJS {
+    NSString *jsString = @"";
+    WKUserScript *userScript = [[WKUserScript alloc] initWithSource: jsString
+                                                      injectionTime: WKUserScriptInjectionTimeAtDocumentEnd
+                                                   forMainFrameOnly:YES];
+    WKUserContentController *wkUController = [WKUserContentController new];
+    [wkUController addUserScript: userScript];
+    
+    [wkUController addScriptMessageHandler:self name:@"callbackHandler"];
+    
+    WKWebViewConfiguration *wkWebConfig = [WKWebViewConfiguration new];
+    wkWebConfig.userContentController = wkUController;
+    
+    return wkWebConfig;
+}
+
++ (void)triggerJS:(NSString *)jsString webView:(WKWebView *)webView {
+    [webView evaluateJavaScript:jsString
+              completionHandler:^(NSString *result, NSError *error){
+                  if (error != nil) {
+                      return;
+                  }
+              }];
+}
+
+@ene
