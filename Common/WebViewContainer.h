f5540b871cac17344c50a4caec1ee5715f6ee08c
kdp <kdp7584@gmail.com>
2020-09-18 20:24:46 +0900

    Update WebViewContainer.h

--- Common/WebViewContainer.h
+++ Common/WebViewContainer.h
@@ -3,7 +3,8 @@
 #import <WebKit/WebKit.h>
 
 @interface WebViewContainer : NSObject
-
+  
++ (void) loadURL(NSString *)requestURLString:(WKWebView *) webView;
 + (void)triggerJS:(NSString *)jsString webView:(WKWebView *)webView;
 + (void)setURL:(NSString *)requestURLString:(WKWebView *) webView;
 + (WKWebViewConfiguration *)setJS;
